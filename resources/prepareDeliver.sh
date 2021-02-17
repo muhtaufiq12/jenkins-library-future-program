e657a8f7856a1ce2c5832f14f8968fbd4dad5ef2
Yohanna Fransiska Aladina <42995520+yohannafrans@users.noreply.github.com>
2021-02-18 01:07:07 +0700

    Update prepareDeliver.sh

--- resources/prepareDeliver.sh
+++ resources/prepareDeliver.sh
@@ -2,7 +2,7 @@
 # Setup Kubernetes Cluster ----------------------------------------------------------------
 kubectl config set-cluster k8s --server=${KUBE_ENDPOINT} --insecure-skip-tls-verify=true \
 && kubectl config set-credentials jenkins --token=${KUBE_TOKEN} \
-&& kubectl config set-context default --cluster=k8s --user=centos \
+&& kubectl config set-context default --cluster=k8s --user=jenkins \
 && kubectl config use-context default
 # ---
 # Create namespace if not exist -----------------------------------------------------------
