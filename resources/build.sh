88af4e5be7536698d5db50b55dee36aadca2bb01
Yohanna Fransiska Aladina <42995520+yohannafrans@users.noreply.github.com>
2021-02-18 00:57:53 +0700

    Update build.sh

--- resources/build.sh
+++ resources/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Running as $(whoami) user."
+echo "Running in $(uname -a) node"
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USER} --password-stdin
 docker build . \
   --tag ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:${CONTAINER_VERSION} \
